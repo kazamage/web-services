09117b020cf216c641b2338ce31c238bbf17403f
kazamage <kazamage1980@gmail.com>
2015-09-06 22:10:47 +0900

    Initial commit

---
+++ src/main/java/jp/pgw/develop/swallow/webservices/Sample.java
@@ -0,0 +1,15 @@
+package jp.pgw.develop.swallow.webservices;
+
+import javax.jws.WebService;
+
+/**
+ * Created by Takeshi on 2015/09/06.
+ */
+@WebService
+public class Sample {
+
+    public void message() {
+        System.out.println("message");
+    }
+
+}
